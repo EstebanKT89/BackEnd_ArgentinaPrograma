c73ab74e32e54a9b890fb10ecae6bfd8bdc714a6
Esteban Gallar <Esteban_2189@hotmail.com>
2022-10-14 22:38:32 -0300

    commit en Heroku para crear una persona

--- src/main/java/com/portfolio/egallar/Controller/PersonaController.java
+++ src/main/java/com/portfolio/egallar/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,18 +51,18 @@ public class PersonaController {
 //        return new ResponseEntity(new Mensaje("Educación eliminada"), HttpStatus.OK);
 //    }
     
-//    @PostMapping("/create")
-//    public ResponseEntity<?> create(@RequestBody DtoPersona dtoPersona){
-//        if(StringUtils.isBlank(dtoPersona.getNombreEduc())){
-//            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-//        }
-//        if(impEducacionService.existsByNombreEduc(dtoEducacion.getNombreEduc())){
-//            return new ResponseEntity(new Mensaje("El nombre ingresado ya existe"), HttpStatus.BAD_REQUEST);
-//        }
-//        Educacion educacion = new Educacion(dtoEducacion.getNombreEduc(), dtoEducacion.getDescripcionEduc());
-//        impEducacionService.save(educacion);
-//        return new ResponseEntity(new Mensaje("Educación creada"), HttpStatus.OK);
-//    }
+    @PostMapping("/crear")
+    public ResponseEntity<?> create(@RequestBody DtoPersona dtoPersona){
+        if(StringUtils.isBlank(dtoPersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(impPersonaService.existsByNombre(dtoPersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre ingresado ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        Persona persona = new Persona(dtoPersona.getNombre(),dtoPersona.getApellido(), dtoPersona.getDescripcion(), dtoPersona.getImg());
+        impPersonaService.save(persona);
+        return new ResponseEntity(new Mensaje("Educación creada"), HttpStatus.OK);
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtoPersona){
