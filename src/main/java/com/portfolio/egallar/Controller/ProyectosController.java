7f10e0bc9ed676b6f841ed24114c1bfe2b04854c
Esteban Gallar <Esteban_2189@hotmail.com>
2022-10-15 15:23:08 -0300

    Commit en Heroku con actualizacion de imagen de perfil

--- src/main/java/com/portfolio/egallar/Controller/ProyectosController.java
+++ src/main/java/com/portfolio/egallar/Controller/ProyectosController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyectos")
-@CrossOrigin(origins = {"https://frontendegallar.web.app"})
+@CrossOrigin(origins = {"https://frontendegallar.web.app","http://localhost:4200"})
 public class ProyectosController {
 
     @Autowired
